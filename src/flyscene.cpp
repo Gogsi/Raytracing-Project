3b8a58b74b2e0f30598eef8a185d52738f8d3f3a
Georgi Genkov <gogogenkov@gmail.com>
2019-10-25 11:51:09 +0200

    New given code now works

--- src/flyscene.cpp
+++ src/flyscene.cpp
@@ -9,49 +9,43 @@ using namespace std;
 #define MAX_BOUNCES 2
 
 void Flyscene::initialize(int width, int height) {
-  // initiliaze the Phong Shading effect for the Opengl Previewer
-  phong.initialize();
+	// initiliaze the Phong Shading effect for the Opengl Previewer
+	phong.initialize();
 
-  // set the camera's projection matrix
-  flycamera.setPerspectiveMatrix(60.0, width / (float)height, 0.1f, 100.0f);
-  flycamera.setViewport(Eigen::Vector2f((float)width, (float)height));
+	// set the camera's projection matrix
+	flycamera.setPerspectiveMatrix(60.0, width / (float)height, 0.1f, 100.0f);
+	flycamera.setViewport(Eigen::Vector2f((float)width, (float)height));
 
-  // load the OBJ file and materials
-  Tucano::MeshImporter::loadObjFile(mesh, materials,
-                                    "resources/models/cube.obj");
-
-
-  // normalize the model (scale to unit cube and center at origin)
-  mesh.normalizeModelMatrix();
-
-  // pass all the materials to the Phong Shader
-  for (int i = 0; i < materials.size(); ++i)
-    phong.addMaterial(materials[i]);
+	// load the OBJ file and materials
+	Tucano::MeshImporter::loadObjFile(mesh, materials,
+		"resources/models/twoObjects.obj");
 
 
-  for (int i = 0; i < mesh.getNumberOfFaces(); ++i) {
-	  triangles.push_back(mesh.getFace(i));
-  }
+	mesh.normalizeModelMatrix();
 
+	// pass all the materials to the Phong Shader
+	for (int i = 0; i < materials.size(); ++i)
+		phong.addMaterial(materials[i]);
 
-  // set the color and size of the sphere to represent the light sources
-  // same sphere is used for all sources
-  lightrep.setColor(Eigen::Vector4f(1.0, 1.0, 0.0, 1.0));
-  lightrep.setSize(0.15);
+	// set the color and size of the sphere to represent the light sources
+	// same sphere is used for all sources
+	lightrep.setColor(Eigen::Vector4f(1.0, 1.0, 0.0, 1.0));
+	lightrep.setSize(0.15);
 
-  // create a first ray-tracing light source at some random position
-  lights.push_back(Eigen::Vector3f(-1.0, 1.0, 1.0));
+	// create a first ray-tracing light source at some random position
+	lights.push_back(Eigen::Vector3f(-0.5, 2.0, 3.0));
 
-  // scale the camera representation (frustum) for the ray debug
-  camerarep.shapeMatrix()->scale(0.2);
+	// scale the camera representation (frustum) for the ray debug
+	camerarep.shapeMatrix()->scale(0.2);
 
-  // the debug ray is a cylinder, set the radius and length of the cylinder
-  ray.setSize(0.005, 10.0);
+	// the debug ray is a cylinder, set the radius and length of the cylinder
+	ray.setSize(0.005, 1.0);
 
-  // craete a first debug ray pointing at the center of the screen
-  createDebugRay(Eigen::Vector2f(width / 2.0, height / 2.0));
+	// craete a first debug ray pointing at the center of the screen
+	createDebugRay(Eigen::Vector2f(width / 2.0, height / 2.0));
 
-  glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
+
 
   // for (int i = 0; i<mesh.getNumberOfFaces(); ++i){
   //   Tucano::Face face = mesh.getFace(i);    
@@ -65,76 +59,79 @@ void Flyscene::initialize(int width, int height) {
   // }
 
   // create the array of boxes
+	for (int i = 0; i < mesh.getNumberOfFaces(); ++i) {
+		triangles.push_back(mesh.getFace(i));
+	}
+
   Box box = Box(mesh);
   this->boxes = divideBox(box, 8);
 }
 
 void Flyscene::paintGL(void) {
-  
 
-  // update the camera view matrix with the last mouse interactions
-  flycamera.updateViewMatrix();
-  Eigen::Vector4f viewport = flycamera.getViewport();
+	// update the camera view matrix with the last mouse interactions
+	flycamera.updateViewMatrix();
+	Eigen::Vector4f viewport = flycamera.getViewport();
 
-  // clear the screen and set background color
-  glClearColor(0.9, 0.9, 0.9, 0.0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// clear the screen and set background color
+	glClearColor(0.9, 0.9, 0.9, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  // position the scene light at the last ray-tracing light source
-  scene_light.resetViewMatrix();
-  scene_light.viewMatrix()->translate(-lights.back());
+	// position the scene light at the last ray-tracing light source
+	scene_light.resetViewMatrix();
+	scene_light.viewMatrix()->translate(-lights.back());
 
-  // render the scene using OpenGL and one light source
-  phong.render(mesh, flycamera, scene_light);
+	// render the scene using OpenGL and one light source
+	phong.render(mesh, flycamera, scene_light);
 
-  // render the ray and camera representation for ray debug
-  ray.render(flycamera, scene_light);
-  camerarep.render(flycamera, scene_light);
+	// render the ray and camera representation for ray debug
+	ray.render(flycamera, scene_light);
+	camerarep.render(flycamera, scene_light);
 
-  // render ray tracing light sources as yellow spheres
-  for (int i = 0; i < lights.size(); ++i) {
-    lightrep.resetModelMatrix();
-    lightrep.modelMatrix()->translate(lights[i]);
-    lightrep.render(flycamera, scene_light);
-  }
+	// render ray tracing light sources as yellow spheres
+	for (int i = 0; i < lights.size(); ++i) {
+		lightrep.resetModelMatrix();
+		lightrep.modelMatrix()->translate(lights[i]);
+		lightrep.render(flycamera, scene_light);
+	}
 
-  // render coordinate system at lower right corner
-  flycamera.renderAtCorner();
+	// render coordinate system at lower right corner
+	flycamera.renderAtCorner();
 }
 
-void Flyscene::simulate(GLFWwindow *window) {
-  // Update the camera.
-  // NOTE(mickvangelderen): GLFW 3.2 has a problem on ubuntu where some key
-  // events are repeated: https://github.com/glfw/glfw/issues/747. Sucks.
-  float dx = (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS ? 0.1 : 0.0) -
-             (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS ? 0.1 : 0.0);
-  float dy = (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS ||
-                      glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS
-                  ? 0.1
-                  : 0.0) -
-             (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS ||
-                      glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS
-                  ? 0.1
-                  : 0.0);
-  float dz = (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS ? 0.1 : 0.0) -
-             (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS ? 0.1 : 0.0);
-  flycamera.translate(dx, dy, dz);
+void Flyscene::simulate(GLFWwindow* window) {
+	// Update the camera.
+	// NOTE(mickvangelderen): GLFW 3.2 has a problem on ubuntu where some key
+	// events are repeated: https://github.com/glfw/glfw/issues/747. Sucks.
+	float dx = (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS ? 0.1 : 0.0) -
+		(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS ? 0.1 : 0.0);
+	float dy = (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS ||
+		glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS
+		? 0.1
+		: 0.0) -
+		(glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS ||
+			glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS
+			? 0.1
+			: 0.0);
+	float dz = (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS ? 0.1 : 0.0) -
+		(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS ? 0.1 : 0.0);
+	flycamera.translate(dx, dy, dz);
 }
 
-void Flyscene::createDebugRay(const Eigen::Vector2f &mouse_pos) {
-  ray.resetModelMatrix();
-  // from pixel position to world coordinates
-  Eigen::Vector3f screen_pos = flycamera.screenToWorld(mouse_pos);
+void Flyscene::createDebugRay(const Eigen::Vector2f& mouse_pos) {
+	ray.resetModelMatrix();
+	// from pixel position to world coordinates
+	Eigen::Vector3f screen_pos = flycamera.screenToWorld(mouse_pos);
 
-  // direction from camera center to click position
-  Eigen::Vector3f dir = (screen_pos - flycamera.getCenter()).normalized();
-  
-  // position and orient the cylinder representing the ray
-  ray.setOriginOrientation(flycamera.getCenter(), dir);
+	// direction from camera center to click position
+	Eigen::Vector3f dir = (screen_pos - flycamera.getCenter()).normalized();
 
-  // place the camera representation (frustum) on current camera location, 
-  camerarep.resetModelMatrix();
-  camerarep.setModelMatrix(flycamera.getViewMatrix().inverse());
+	// position and orient the cylinder representing the ray
+	ray.setOriginOrientation(flycamera.getCenter(), dir);
+
+	// place the camera representation (frustum) on current camera location, 
+	camerarep.resetModelMatrix();
+	camerarep.setModelMatrix(flycamera.getViewMatrix().inverse());
 }
 
 void Flyscene::raytraceScene(int width, int height) {
@@ -189,7 +186,7 @@ void Flyscene::updating_pixels(vector<vector<Eigen::Vector3f>>& pixel_data, Eige
 			// create a ray from the camera passing through the pixel (i,j)
 			Eigen::Vector3f screen_coords = flycamera.screenToWorld(Eigen::Vector2f(i, j));
 			// launch raytracing for the given ray and write result to pixel data
-			pixel_data[i][j] = traceRay(0, Ray(origin, screen_coords - origin));
+			pixel_data[j][i] = traceRay(0, Ray(origin, screen_coords - origin));
 		}
 	}
 }
@@ -218,12 +215,13 @@ Eigen::Vector3f Flyscene::traceRay(int bounce, Ray ray) {
 			}
 		}
 	}
+
 	//std::cout << "Box hit: " << i << std::endl;
 	if (smallestT != INFINITY) {
 		return Shader(bounce, closest_triangle, smallestHit, ray);
 	}
 
-	if(bounce == 0) return Eigen::Vector3f(1.0, 1.0, 1.0);
+	if(bounce == 0) return Eigen::Vector3f(0.0, 0.0, 1.0);
 	return Eigen::Vector3f(0.0, 0.0, 0.0);
 }
 
@@ -265,7 +263,7 @@ Eigen::Vector3f Flyscene::Shader(int bounce, Tucano::Face face, HitInfo hit, Ray
 
 	Eigen::Vector3f ambient = mat.getAmbient().cwiseProduct(lightIntensity);
 
-	Eigen::Vector3f diffuse = std::max(normalN.dot(lightDirection), 0.0f) * mat.getDiffuse().cwiseProduct( lightIntensity);
+	Eigen::Vector3f diffuse = std::max(normalN.dot(lightDirection), 0.0f) * mat.getDiffuse().cwiseProduct(lightIntensity);
 
 	Eigen::Vector3f specular = multiply(lightIntensity, mat.getSpecular()) * std::pow(std::max(dotted, 0.0f), mat.getShininess());
 
@@ -341,7 +339,7 @@ HitInfo Flyscene::intersectPlane(Eigen::Vector3f& origin,
 
 			Eigen::Vector3f hit = origin + t * dir;
 
-			if (t >= 1E-6) {
+			if (t >= 1E-9) {
 				if (t < smallestT) {
 					smallestT = t;
 					smallestFace = i;
